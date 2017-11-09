86b96d92c0022b2bd79b1e992852fa869c4fb9c6
Liang <gaoxiaoliangz@users.noreply.github.com>
2017-11-10 01:02:32 +0800

    no new

--- src/main.js
+++ src/main.js
@@ -2,28 +2,50 @@ import { guid, last } from './utils'
 
 const defaultConfig = {
   container: 'body',
-  tocTag: ['{{TOC}}', '[TOC]']
+  match: /\{\{TOC\}\}/g,
+  inputHTML: '',
 }
 
 const OUTLINE_TAGS = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6']
-const OUTLINE_DIV = 'otl-toc'
+const TOC_EL = 'otl-toc'
 
 function Outline(config) {
+  return new _Outline(config)
+}
+
+Outline.config = defaultConfig
+
+Outline.buildTOC = function buildTOC($container) {
+  return genTOCDom(buildStructure(markTags(findHTags($container))))
+}
+
+Outline.injectTOC = function injectTOC($container, match, $toc) {
+  $container.innerHTML = $container.innerHTML.replace(match, $toc.outerHTML)
+}
+
+function _Outline(config) {
   this.config = Object.assign({}, defaultConfig, config)
-  this.$container = document.querySelector(this.config.container)
+  this.match = this.config.match
+  this.$container = typeof this.config.container === 'string'
+    ? document.querySelector(this.config.container)
+    : this.config.container
   this.update()
 }
 
-Outline.prototype._generateTOCDom = function _generateTOCDom() {
+_Outline.prototype._generateTOCDom = function _generateTOCDom() {
   return genTOCDom(buildStructure(markTags(findHTags(this.$container))))
 }
 
-Outline.prototype.update = function update() {
+_Outline.prototype.hasTocTag = function hasTocTag() {
+  return this.match.test(this.$container.innerHTML)
+}
+
+_Outline.prototype.update = function update() {
   const $toc = this._generateTOCDom()
-  if (document.querySelector(`.${OUTLINE_DIV}`)) {
-    document.querySelector(`.${OUTLINE_DIV}`).innerHTML = $toc.innerHTML
-  } else {
-    this.$container.innerHTML = this.$container.innerHTML.replace(this.config.tocTag[0], $toc.outerHTML)
+  if (document.querySelector(`.${TOC_EL}`)) {
+    document.querySelector(`.${TOC_EL}`).innerHTML = $toc.innerHTML
+  } else if (this.hasTocTag()) {
+    this.$container.innerHTML = this.$container.innerHTML.replace(this.match, $toc.outerHTML)
   }
 }
 
@@ -126,7 +148,7 @@ function genTOCDom(sections) {
 
   const $toc = document.createElement('div')
   const $tocWrap = document.createElement('div')
-  $toc.setAttribute('class', OUTLINE_DIV)
+  $toc.setAttribute('class', TOC_EL)
   $tocWrap.setAttribute('class', 'otl-list')
   $tocWrap.appendChild(createChildren(sections))
   $toc.appendChild($tocWrap)
